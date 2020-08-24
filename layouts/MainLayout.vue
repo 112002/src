8721bdb3627c1b05b91a89ff4abc31911ea848f4
112002 <59130972+112002@users.noreply.github.com>
2020-08-24 22:07:04 +0530

    init

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -1,23 +1,6 @@
 <template>
   <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="leftDrawerOpen = !leftDrawerOpen"
-        />
-
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+    
 
     <q-drawer
       v-model="leftDrawerOpen"
@@ -47,7 +30,6 @@
 </template>
 
 <script>
-import EssentialLink from 'components/EssentialLink.vue'
 
 const linksData = [
   {
